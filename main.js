acbeb986c3986817d27aabde2ad2ad22a01ba10b
Gustavo Avila <105921907+GustavoAvila646@users.noreply.github.com>
2023-11-30 07:46:10 -0300

    pinto

--- main.js
+++ main.js
@@ -13,5 +13,11 @@ while(contador < listaDeTeclas.length){
     }
     contador = contador + 1;
     console.log(contador);
+
+    listaDeTeclas.onkeydown = function(evento){
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            listaDeTeclas.classlist.add('ativa')
+        }
+    }
     
 }
